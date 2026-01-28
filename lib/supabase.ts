50a60d751e1989bb802b5135dd7d619f9866c632
cpamarketing00691-stack <cpamarketing00691@gmail.com>
2026-01-29 00:46:58 +0600

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://uycxbrcbweeuvizrgpnw.supabase.co';
-const supabaseAnonKey = 'sb_publishable_AvaNpR5XsRFbhSu7A6uHhg_W2xzhkRx';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5Y3hicmNid2VldXZpenJncG53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MTQ0MzcsImV4cCI6MjA4NTA5MDQzN30.PMtYejPByxn1lWf-946DvgTn1hId5Yb4JoRpM-inxsU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
